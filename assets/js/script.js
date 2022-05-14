c69031a8a450da52c9a7ece8b37c7e0a0dfbd307
AJDewy <adam32thomas@gmail.com>
2022-05-14 17:13:09 -0400

    4.2.7 progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,22 @@ let createTaskHandler = function(event) {
 
     event.preventDefault();
 
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // console.dir(taskNameInput);
+
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
-// let buttonElClick.addEventListener("click", insert task)
 
 formEl.addEventListener("submit", createTaskHandler);
